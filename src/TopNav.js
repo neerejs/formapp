9f5907d46c2a3231ee1ab920683f7d307722b2b9
Neerej Selvakumar <neerejselvakumar@Neerejs-MacBook-Pro.local>
2022-06-18 17:34:31 -0400

    forms

---
+++ src/TopNav.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import neerejLogo from './Neerej.png'
+import { LinkContainer } from 'react-router-bootstrap'
+
+
+
+const TopNav = () => {
+   
+    return (
+        <>
+            <div>
+
+                <Navbar bg="dark" expand="lg" variant="dark">
+                    <Container>
+                        <Navbar.Brand href="#home">
+
+                        <img
+                                alt=""
+                                src={neerejLogo}
+                                width="50"
+                                height="50"
+                                className="d-inline-block align-top"
+                            />
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <LinkContainer to="/">
+                                    <Nav.Link >Home</Nav.Link>
+                                </LinkContainer>
+                                
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+
+            </div>
+
+        </>
+    )
+}
+
+export default TopNav;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import TopNav from './TopNav';
+import FormikForm from './FormikForm';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <BrowserRouter>
+      <TopNav />
+      <Routes>
+      <Route path="/" element={<FormikForm />}></Route>
+      </Routes>
+  </BrowserRouter>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
