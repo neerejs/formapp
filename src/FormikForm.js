8491a2972e11b7c7fece209f7bd48d2102ddcf7c
Neerej Selvakumar <neerejselvakumar@Neerejs-MacBook-Pro.local>
2022-06-20 17:51:32 -0400

    forms done

--- src/FormikForm.js
+++ src/FormikForm.js
@@ -1,12 +1,14 @@
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 import * as contentfulManagement from 'contentful-management';
-import { Formik } from 'formik';
+// import { Formik } from 'formik';
+import { Formik, Field } from 'formik';
 import * as Yup from 'yup';
 import './FormikForm.css';
-import { Col, Row, Container } from 'react-bootstrap';
 
 const FormikForm = () => {
+
+
 
     const validationSchema = Yup.object().shape({
         name: Yup.string()
@@ -17,193 +19,230 @@ const FormikForm = () => {
             .email("*Must be a valid email address")
             .max(100, "*Email must be less than 100 characters")
             .required("*Email is required"),
-        lastname: Yup.string()
+        address: Yup.string()
             .min(2, "*Names must have at least 2 characters")
             .max(100, "*Names can't be longer than 100 characters")
-            .required("*Last Name is required"),
-        comments: Yup.string()
-            .min(2, "*Names must have at least 2 characters")
-            .max(100, "*Names can't be longer than 100 characters")
-            .required("*Comments is required")
-
+            .required("*Name is required"),
+        checked: Yup.array().min(1),
+        radios: Yup.string().oneOf(["Green", "Red", "Blue"]).required(),
+        cars: Yup.string().required("*Car model  is required"),
     });
 
     return (
-        <>
-            <Container>
-                <Row>
-                    <Col>
-                        <Formik initialValues={{ name: "", lastname: "", email: "", comments: "", grade: "" }}
-                            validationSchema={validationSchema}
-                            onSubmit={(values, { setSubmitting, resetForm }) => {
-                                console.log(values)
 
-                                setSubmitting(true)
-                                setTimeout(() => {
-                                    alert(JSON.stringify(values, null, 6));
-
-                                    const client = contentfulManagement.createClient({
-                                        accessToken: 'CFPAT-oUCFauW8e8B0z1etnV5Lx0V0eEegb51Dy4d__550f1k'
-                                    })
-
-
-                                    client.getSpace('nvm4509pk8bp')
-                                        .then((space) => space.getEnvironment('master'))
-                                        .then((environment) => environment.createEntry('giveaway', {
-                                            fields: {
-                                                name: {
-                                                    'en-US': values.name
-                                                },
-                                                email: {
-                                                    'en-US': values.email
-                                                },
-                                                lastname: {
-                                                    'en-US': values.lastname
-                                                },
-                                                comments: {
-                                                    'en-US': values.comments
-                                                },
-                                                grade: {
-                                                    'en-US': values.grade
-                                                }
-
-                                            }
-                                        }))
-                                        .then((entry) => {
-                                            console.log(entry)
-                                            entry.publish()
-                                        })
-                                        .catch((error) => {
-
-                                        })
+        <Formik initialValues={{ name: "", email: "", address: "", checked: [], radios: '', cars: '' }}
+            validationSchema={validationSchema}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+                console.log(values)
+                setSubmitting(true)
 
 
 
 
-                                    resetForm();
-                                    setSubmitting(false);
-                                }, 500);
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+
+                    const client = contentfulManagement.createClient({
+                        accessToken: 'CFPAT-oUCFauW8e8B0z1etnV5Lx0V0eEegb51Dy4d__550f1k'
+                    })
 
 
-                                // setSubmiting(true);
+                    client.getSpace('nvm4509pk8bp')
+                        .then((space) => space.getEnvironment('master'))
+                        .then((environment) => environment.createEntry('giveaway', {
+                            fields: {
+                                name: {
+                                    'en-US': values.name
+                                },
+                                email: {
+                                    'en-US': values.email
+                                },
+                                address: {
+                                    'en-US': values.cars
+                                },
+                                lastname: {
+                                    'en-US': values.lastname
+                                },
+                                comments: {
+                                    'en-US': values.comments
+                                },
+                                checked: {
+                                    'en-US': values.checked.toString()
+                                },
+                                radios: {
+                                    'en-US': values.radios
+                                },
+                                cars: {
+                                    'en-US': values.cars
+                                }
+
+                            }
+                        }))
+                        .then((entry) => {
+                            console.log(entry)
+                            entry.publish()
+                        })
+                        .catch((error) => {
+
+                        })
 
 
+                    resetForm();
 
-                            }}
-                        >
+                    setSubmitting(false);
+                }, 500);
 
-                            {({ values, handleChange, handleSubmit, handleBlur, touched, errors, isSubmitting
-                            }) => (
 
-                                <>
-                                    <Form onSubmit={handleSubmit}>
-                                        <Form.Group className="mb-3" controlId="formName">
-                                            <Form.Label>First Name</Form.Label>
-                                            <Form.Control type="text" placeholder="Enter Name"
-                                                name="name"
-                                                onChange={handleChange}
-                                                value={values.name}
-                                                onBlur={handleBlur}
-                                                className={touched.name && errors.name ? "error" : null}
-                                            />
-                                            {touched.name && errors.name ? (
-                                                <div className="error-message">{errors.name}</div>
-                                            ) : null}
-
-                                        </Form.Group>
-
-                                        <Form.Group className="mb-3" controlId="formLastName">
-                                            <Form.Label>Last Name</Form.Label>
-                                            <Form.Control type="text" placeholder="Enter Last Name"
-                                                name="lastname"
-                                                onChange={handleChange}
-                                                value={values.lastname}
-                                                onBlur={handleBlur}
-                                                className={touched.lastname && errors.lastname ? "error" : null}
-                                            />
-                                            {touched.lastname && errors.lastname ? (
-                                                <div className="error-message">{errors.lastname}</div>
-                                            ) : null}
-                                        </Form.Group>
-
-                                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                                            <Form.Label>Email address</Form.Label>
-                                            <Form.Control type="email" placeholder="Enter email"
-                                                name="email"
-                                                onChange={handleChange}
-                                                value={values.email}
-                                                onBlur={handleBlur}
-                                                className={touched.email && errors.email ? "error" : null}
-                                            />
-                                            {touched.email && errors.email ? (
-                                                <div className="error-message">{errors.email}</div>
-                                            ) : null}
-                                        </Form.Group>
-
-                                        <Form.Group className="mb-3" controlId="formComments">
-                                            <Form.Label>Comments</Form.Label>
-                                            <Form.Control as="textarea" type="text" placeholder="Enter Comments"
-                                                name="comments"
-                                                onChange={handleChange}
-                                                value={values.comments}
-                                                onBlur={handleBlur}
-                                                className={touched.comments && errors.comments ? "error" : null}
-                                            />
-                                            {touched.comments && errors.comments ? (
-                                                <div className="error-message">{errors.comments}</div>
-                                            ) : null}
-                                        </Form.Group>
-
-                                        <Form.Group className="mb-3" controlId="formRadio">
-
-                                            {/* <label htmlFor="email" style={{ display: "block" }}>
-                                                Email
-                                            </label>
-
-                                            <label>
-                                                <input
-                                                    type="radio"
-                                                    name="grade"
-                                                    value={values.grade}
-                                                    checked={values.test === "a"}
-                                                    onChange={handleChange()}
-                                                />a
-                                            </label> */}
-
-                                            <Form.Check
-                                                inline
-                                                label="9th grade"
-                                                name="grade"
-
-                                                onChange={handleChange}
-                                                value="9th grade"
-                                                onBlur={handleBlur}
-                                            />
+                // setSubmiting(true);
 
 
 
+            }}
+        >
 
-                                        </Form.Group>
+            {({ values, handleChange, handleSubmit, handleBlur, touched, errors, isSubmitting
+            }) => (
 
-                                        <Button variant="primary" type="submit" disabled={isSubmitting}>
-                                            Submit
-                                    </Button>
-                                    </Form>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Name"
+                            name="name"
+                            onChange={handleChange}
+                            value={values.name}
+                            onBlur={handleBlur}
+                            className={touched.name && errors.name ? "error" : null}
+                        />
+                        {touched.name && errors.name ? (
+                            <div className="error-message">{errors.name}</div>
+                        ) : null}
+
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email"
+                            name="email"
+                            onChange={handleChange}
+                            value={values.email}
+                            onBlur={handleBlur}
+                            className={touched.email && errors.email ? "error" : null}
+                        />
+                        {touched.email && errors.email ? (
+                            <div className="error-message">{errors.email}</div>
+                        ) : null}
+                    </Form.Group>
 
 
-                                </>
-                            )}
-                        </Formik>
+                    <Form.Group className="mb-3" controlId="formBasicAddress">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Address"
+                            name="address"
+                            onChange={handleChange}
+                            value={values.address}
+                            onBlur={handleBlur}
+                            className={touched.address && errors.address ? "error" : null}
+                        />
+                        {touched.address && errors.address ? (
+                            <div className="error-message">{errors.address}</div>
+                        ) : null}
+                    </Form.Group>
 
-                    </Col>
-                </Row>
 
-            </Container>
-        </>
+                    <div id="checkbox-group">Checked</div>
+                    <Form.Group className="mb-3" controlId="formBasicCheck">
+                        <div class="form-check">
+                            <Field type="checkbox" class="form-check-input" name="checked" value="Yes"
+                                onChange={handleChange}
+                                onBlur={handleBlur} />
+                            <label class="form-check-label" for="flexCheckIndeterminate">
+                                Yes
+                            </label>
+                        </div>
+
+                        <div class="form-check">
+                            <Field type="checkbox" class="form-check-input" name="checked" value="No"
+                                onChange={handleChange}
+                                onBlur={handleBlur} />
+                            <label class="form-check-label" for="flexCheckIndeterminate">
+                                No
+                            </label>
+                        </div>
+
+                        <div class="form-check">
+                            <Field type="checkbox" class="form-check-input" name="checked" value="May Be"
+                                onChange={handleChange}
+                                onBlur={handleBlur} />
+                            <label class="form-check-label" for="flexCheckIndeterminate">
+                                May Be
+                            </label>
+                        </div>
+                        {touched.checked && errors.checked ? (
+                            <div className="error-message">{errors.checked}</div>
+                        ) : null}
+                    </Form.Group>
+
+
+                    <div id="checkbox-group">Radios</div>
+                    <Form.Group className="mb-3" controlId="formBasicCheck">
+                        <div class="form-check">
+                            <Field type="radio" class="form-check-input" name="radios" value="Blue"
+                                onChange={handleChange}
+                                onBlur={handleBlur} />
+                            <label class="form-check-label" for="flexCheckIndeterminate">
+                                Blue
+                            </label>
+
+                        </div>
+
+                        <div class="form-check">
+                            <Field type="radio" class="form-check-input" name="radios" value="Green"
+                                onChange={handleChange}
+                                onBlur={handleBlur} />
+                            <label class="form-check-label" for="flexCheckIndeterminate">
+                                Green
+                            </label>
+                        </div>
+
+                        <div class="form-check">
+                            <Field type="radio" class="form-check-input" name="radios" value="Red"
+                                onChange={handleChange}
+                                onBlur={handleBlur} />
+                            <label class="form-check-label" for="flexCheckIndeterminate">
+                                Red
+                            </label>
+                        </div>
+                        {touched.radios && errors.radios ? (
+                            <div className="error-message">{errors.radios}</div>
+                        ) : null}
+                    </Form.Group>
+
+                    <Form.Select aria-label="Default select example"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        name="cars"
+                    >
+
+                        <option>Select the Car Model</option>
+                        <option value="Honda">Honda</option>
+                        <option value="Tesla">Tesla</option>
+                        <option value="Ford">Ford</option>
+                    </Form.Select>
+                    {touched.cars && errors.cars ? (
+                        <div className="error-message">{errors.cars}</div>
+                    ) : null}
+
+
+
+
+                    <Button variant="primary" type="submit" disabled={isSubmitting}>
+                        Submit
+                    </Button>
+                </Form>
+            )}
+        </Formik>
 
 
     );
-
 }
 
 export default FormikForm;
